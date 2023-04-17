76834935b639455fd7a7ad6d6b4caa5f84017f3c
fgvlasov <fgvlasov@gmail.com>
2023-04-17 11:01:11 +0300

    paths 7

--- next.config.js
+++ next.config.js
@@ -4,16 +4,9 @@ const nextConfig = {
   images: {
     loader: "default",
     //domains: ["localhost"],
-	//domains: [process.env.NEXT_PUBLIC_STRAPI_API_URL || "localhost"],
+	domains: [process.env.NEXT_PUBLIC_STRAPI_API_URL,  "localhost"],
 	//path: process.env.NEXT_PUBLIC_STRAPI_API_URL,
-	remotePatterns: [
-		{
-		  protocol: 'https',
-		  hostname: process.env.NEXT_PUBLIC_STRAPI_API_URL,
-		  port: '',
-		  pathname: '/',
-		},
-	  ],
+
   },
 }
 
