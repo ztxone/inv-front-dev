be5ba6e0ad2e225f234eec24a0c6cff5494f1d49
fgvlasov <fgvlasov@gmail.com>
2023-04-17 09:29:07 +0300

    error 8

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -10,7 +10,7 @@ import Image from 'next/image';
 
 const Article = ({ article, categories }) => {
   const imageUrl = getStrapiMedia(article.attributes.image);
-  console.log(article.attributes);
+  console.log(imageUrl);
 
   const seo = {
     metaTitle: article.attributes.title,
@@ -22,15 +22,39 @@ const Article = ({ article, categories }) => {
   return (
     <Layout categories={categories.data}>
       <Seo seo={seo} />
-
+      {/*<div
+        id="banner"
+        className="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-padding uk-margin"
+        data-src={imageUrl}
+        data-srcset={imageUrl}
+        data-uk-img
+      >
         <h1>{article.attributes.title}</h1>
-
+      </div>
+	  */}
       <div className="uk-section">
         <div className="uk-container uk-container-small">
           <ReactMarkdown>{article.attributes.content}</ReactMarkdown>
           <hr className="uk-divider-small" />
           <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
-
+            {/*<div>
+              {article.attributes.author.data.attributes.picture && (
+                <Image
+                  src={getStrapiMedia(
+                    article.attributes.author.data.attributes.picture
+                  )}
+                  alt={
+                    article.attributes.author.data.attributes.picture.data
+                      .attributes.alternativeText
+                  }
+                  style={{
+                    position: "static",
+                    borderRadius: "20%",
+                    height: 60,
+                  }}
+                />
+              )}
+				</div>*/}
             <div className="uk-width-expand">
               <p className="uk-margin-remove-bottom">
                 By {article.attributes.author.data.attributes.name}
