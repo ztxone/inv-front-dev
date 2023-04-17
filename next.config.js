191d362a378d93b3a9f1ea969d57c4ac4e77a009
fgvlasov <fgvlasov@gmail.com>
2023-04-17 12:00:21 +0300

    paths 9

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   images: {
     loader: "default",
     //domains: ["localhost"],
-	domains: ["inv.ptzsite.ru"],
-	//path: process.env.NEXT_PUBLIC_STRAPI_API_URL,
-
+	domains: ["inv.ptzsite.ru", "localhost"],
   },
 }
 
