06ea337d0757f230cda405cda53c7216f71755d9
fgvlasov <fgvlasov@gmail.com>
2023-04-17 09:47:28 +0300

    images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "default",
-    domains: ["localhost"],
+    //domains: ["localhost"],
+	domains: [process.env.NEXT_PUBLIC_STRAPI_API_URL],
   },
 }
 
