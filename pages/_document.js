e948f814e534eec7db5d5f29ebce9062a0d6adaa
Elena <pumpkinpum@gmail.com>
2023-05-12 00:11:21 +0300

    fix: style

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {Html, Head, Main, NextScript} from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -14,10 +14,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/uikit@3.10.1/dist/css/uikit.min.css"
           />
-          <script
+          {/* <script
             async
             src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js"
-          />
+          /> */}
           <script
             async
             src="https://cdn.jsdelivr.net/npm/uikit@3.10.1/dist/js/uikit-icons.min.js"
