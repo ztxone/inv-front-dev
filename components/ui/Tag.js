7095750368a56ff2ecba05cb80f9155df9517145
Elena <pumpkinpum@gmail.com>
2023-05-11 22:17:26 +0300

    fix: section Projects

---
+++ components/ui/Tag.js
@@ -0,0 +1,8 @@
+export default function Tag({text}) {
+  return (
+    <div className='bg-white w-fit rounded-full px-[16px] py-[7px] inline-flex items-center'>
+      <span className='dot'></span>
+      <p className='text-xxs tag-text'>{text}</p>
+    </div>
+  )
+}
