41f93d3a487446b29a6967b1093d7c1326eadd8d
fgvlasov <fgvlasov@gmail.com>
2023-05-15 18:26:47 +0300

    deleted services

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import Layout from "../components/layout";
 import {fetchAPI} from "../lib/api";
 import About from '../components/pages/index/About';
-import Services from '../components/pages/index/Services';
 import Projects from '../components/pages/index/Projects';
 import Blog from '../components/pages/index/Blog';
 import {useLayoutEffect} from 'react';
 
-const Home=({projects, services}) => {
+const Home=({projects}) => {
   useLayoutEffect(() => {
     document.body.classList.add("bg-black");
     document.body.classList.add("text-white");
@@ -26,17 +25,15 @@ const Home=({projects, services}) => {
 
 export async function getStaticProps() {
   // Run API calls in parallel
-  const [projectsRes, servicesRes]=await Promise.all([
+  const [projectsRes]=await Promise.all([
 
     fetchAPI("/projects", {populate: "*"}),
-    fetchAPI("/services", {populate: "*"})
 
   ]);
 
   return {
     props: {
       projects: projectsRes.data,
-      services: servicesRes.data,
     },
     revalidate: 1,
   };
