3278f800235f9ef62961c9a2e7d51039a25fb56d
fgvlasov <fgvlasov@gmail.com>
2023-05-20 11:56:40 +0300

    services showed

--- components/ui/ServiceItem.js
+++ components/ui/ServiceItem.js
@@ -3,16 +3,17 @@ import { getStrapiMedia } from "lib/media";
 import Image from 'next/image';
 
 export default function ServiceItem({title, subtitle,
-  descriptionItem1, descriptionItem2, link}) {
+  descriptionItem1, descriptionItem2, image}) {
   return (
     <div className={`services-card md:mb-5 lg:mr-7 relative`}>
       <TitleH3 text={title} subtext={subtitle} />
       <p className='relative z-10 text-lg pb-[2px]'>{descriptionItem1}</p>
       <p className='relative z-10 text-lg'>{descriptionItem2}</p>
-      <div className='absolute top-0 bottom-0 left-0 min-w-[398px] min-h-[600px] lg:min-w-fit'>
-        <Image src={getStrapiMedia(link)} alt={title} width="100" height="100" className='object-cover w-full
+      {image && (<div className='absolute top-0 bottom-0 left-0 min-w-[398px] min-h-[600px] lg:min-w-fit'>
+        <Image src={getStrapiMedia(image)} alt={title} width="100" height="100" className='object-cover w-full
         h-full rounded-5xl'/>
       </div>
+	  )}
     </div>
   )
 }
