6fbffa210d1104c76ad7d4b48feae685b5073379
Elena <pumpkinpum@gmail.com>
2023-06-01 18:45:44 +0300

    fix: add Project and Blog

--- pages/about.js
+++ pages/about.js
@@ -4,9 +4,11 @@ import Layout from '../components/layout';
 // import Projects from '../components/pages/index/Projects';
 import Intro from '@/components/pages/about/Intro';
 import {useLayoutEffect} from 'react';
+import Projects from '../components/pages/index/Projects';
+import Blog from '@/components/pages/index/Blog';
 
 
-export default function About({}) {
+export default function About({projects}) {
   useLayoutEffect(() => {
     document.body.classList.add('bg-whisper');
     document.body.classList.add('text-black-russian');
@@ -14,7 +16,9 @@ export default function About({}) {
   return (
     <Layout>
           <div className='mx-auto'>
-           <Intro/> 
+        <Intro /> 
+        <Projects projects={projects} moreProjects={true} />
+        <Blog />
       </div>
     </Layout>
   );
