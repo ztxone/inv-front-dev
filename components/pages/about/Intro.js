91bed0db07d5999c793a4418b2d2343e38b048e4
fgvlasov <fgvlasov@gmail.com>
2023-06-02 15:37:11 +0300

    about Data

--- components/pages/about/Intro.js
+++ components/pages/about/Intro.js
@@ -8,24 +8,22 @@ import IntroSlides from '../../ui/InroSlides';
 import IntroDescription from '../../ui/IntroDescription';
 import IntroCost from '@/components/ui/IntroCost';
 
-export default function Intro() {
+export default function Intro( {info} ) {
+	
+	const aboutPurpose = JSON.parse(info.attributes.AboutPurpose);
+	const aboutOpportunities = JSON.parse(info.attributes.AboutOpportunities);
+	//console.log(aboutPurpose.blocks[0].data.text);
   return (
     <div className='container border-t border-light-grey pt-20 pb-15 px-3.8 lg:max-w-[1746px]'>
-      <Title text='О студии' variant='white' />
+      <Title text={info.attributes.Title} variant='white' />
 
       <div className='border-t border-light-grey mt-10 py-5'>
-        <BreadCrumbs item1='Главная' link1='/index' itemLast='О студии' />
+        <BreadCrumbs item1='Главная' link1='/index' itemLast={info.attributes.Title} />
       </div>
       <div className='lg:flex flex-wrap justify-between pb-15'>
-      <IntroDescription title='наша основная цель' text='Мы предоставляем услуги по 3D визуализации архитектурных объектов и
-          предметов, создание видео роликов основанных на 3D технологии. Мы
-          имеем мощную техническую базу и большой опыт, поэтому можем предложить
-          конкурентно способный продукт.'></IntroDescription>
+      <IntroDescription title='наша основная цель' text={aboutPurpose.blocks[0].data.text}></IntroDescription>
       <IntroSlides />
-      <IntroDescription title='наши возможности' text='Студия INVERT это сильная команда специалистов в сфере разработки 3D
-          визуализации любой сложности и направления. Наша основная цель
-          создавать только качественный контент для наших клиентов, который
-          полностью удовлетворит их потребности и запросы.'></IntroDescription>
+      <IntroDescription title='наши возможности' text={aboutOpportunities.blocks[0].data.text}></IntroDescription>
       </div>
 
       <IntroCost/>
--- pages/about.js
+++ pages/about.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Layout from '../components/layout';
 // import {fetchAPI} from "../lib/api";
 // import Projects from '../components/pages/index/Projects';
+import {fetchAPI} from "lib/api";
 import Intro from '@/components/pages/about/Intro';
 import {useLayoutEffect} from 'react';
 import Projects from '../components/pages/index/Projects';
 import Blog from '@/components/pages/index/Blog';
 
 
-export default function About({projects}) {
+export default function About({projects, about}) {
   useLayoutEffect(() => {
     document.body.classList.add('bg-whisper');
     document.body.classList.add('text-black-russian');
@@ -16,10 +17,28 @@ export default function About({projects}) {
   return (
     <Layout>
           <div className='mx-auto'>
-        <Intro /> 
+        <Intro info={about}/> 
         <Projects projects={projects} moreProjects={true} />
         <Blog />
       </div>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+	// Run API calls in parallel
+	const [aboutRes]=await Promise.all([
+  
+	  fetchAPI("/about", {
+		  populate: "*",
+	  })  
+	]);
+  
+	return {
+	  props: {
+		about: aboutRes.data,
+	  },
+	  revalidate: 1,
+	};
+  }
+  
