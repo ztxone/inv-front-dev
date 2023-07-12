1a83260a3044aefea3be47e1be0199bf798f50db
Elena <pumpkinpum@gmail.com>
2023-07-12 14:56:18 +0300

    fix: add scroll

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -1,10 +1,10 @@
-import {useEffect} from "react";
-import ReactModal from "react-modal";
+import {useEffect} from 'react';
+import ReactModal from 'react-modal';
 import ButtonClose from '../ui/ButtonClose';
 
 function Modal({isOpen, onClose, children}) {
   useEffect(() => {
-    ReactModal.setAppElement("#__next");
+    ReactModal.setAppElement('#__next');
   }, []);
   const handleClose=() => {
     onClose();
@@ -15,17 +15,16 @@ function Modal({isOpen, onClose, children}) {
       <ReactModal
         isOpen={isOpen}
         onRequestClose={handleClose}
-        className="absolute top-15 z-50 mx-3.8 bg-white rounded-5xl md:max-w-[550px] md:mx-auto"
-        overlayClassName="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-40
-        flex flex-col items-center justify-center overflow-y-auto"
+        className='absolute top-20 z-50 mx-3.8 bg-white rounded-5xl md:max-w-[550px] md:mx-auto'
+        overlayClassName='fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-40
+        flex flex-col items-center justify-center overflow-y-auto'
       >
-        <div className="modal-content">
+        <div className='modal-content max-h-[90vh] overflow-y-scroll'>
           {children}
         </div>
         <ButtonClose onClick={handleClose} />
       </ReactModal>
     </>
-
   );
 }
 export default Modal;
--- components/ui/ModalLabel.js
+++ components/ui/ModalLabel.js
@@ -1,5 +1,5 @@
 export default function ModalLabel({htmlFor, text}) {
-    return (
-        <label className='pb-2 text-fiord text-sl' htmlFor={htmlFor}>{text}</label>
-    )
+  return (
+    <label className='pb-2 text-fiord text-sl' htmlFor={htmlFor}>{text}</label>
+  )
 }
