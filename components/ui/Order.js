2949be9b50c73613563d9b14c52f646b1e265692
Alex Kochnev <madjok87@icloud.com>
2023-08-27 18:48:50 +0200

    feat: add model window to call order

--- components/ui/Order.js
+++ components/ui/Order.js
@@ -1,30 +1,30 @@
-import PillowLink from './PillowLink';
-import useTranslation from 'next-translate/useTranslation';
-import {useEffect, useState} from 'react';
-import Loading from '../ui/Loading';
-import {fetchAPI} from 'lib/api';
-import Modal from './Modal';
-import FormOrder from '../Forms/FormOrder';
+import PillowLink from "./PillowLink";
+import useTranslation from "next-translate/useTranslation";
+import { useEffect, useState } from "react";
+import Loading from "../ui/Loading";
+import { fetchAPI } from "lib/api";
+import Modal from "./Modal";
+import { FormOrder } from "../Forms/FormOrder";
 
 export default function Order() {
-  const [data, setData]=useState();
-  const i18n=useTranslation();
-  const locale=i18n.lang;
+  const [data, setData] = useState();
+  const i18n = useTranslation();
+  const locale = i18n.lang;
 
-  const [modalIsOpen, setModalIsOpen]=useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  const handleOpenModal=() => {
+  const handleOpenModal = () => {
     setModalIsOpen(true);
   };
 
-  const handleCloseModal=() => {
+  const handleCloseModal = () => {
     setModalIsOpen(false);
   };
 
   useEffect(() => {
     async function fetchData() {
-      const contactRes=await fetchAPI('/contact', {
-        fields: ['Phone', 'PhoneLink'],
+      const contactRes = await fetchAPI("/contact", {
+        fields: ["Phone", "PhoneLink"],
         locale: locale,
       });
 
@@ -38,18 +38,18 @@ export default function Order() {
   }
 
   return (
-    <div className='hidden md:flex items-center min-w-fit'>
+    <div className="hidden md:flex items-center min-w-fit">
       <a
         href={`tel:${data.PhoneLink}`}
-        className='p-2.5 mr-9 tracking-tight  hover:text-suva-grey'
+        className="p-2.5 mr-9 tracking-tight  hover:text-suva-grey"
       >
         {data.Phone}
       </a>
       <div onClick={handleOpenModal}>
         <PillowLink
-          text='Отправить заявку'
-          variant='white'
-          variantSvg='black'
+          text="Отправить заявку"
+          variant="white"
+          variantSvg="black"
         />
       </div>
       <Modal isOpen={modalIsOpen} onClose={handleCloseModal}>
