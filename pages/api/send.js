f3bc4146f9ae529406ddebd89c6d111cdf42df7c
Alex Kochnev <madjok87@icloud.com>
2023-09-18 16:55:01 +0200

    feat:fix logs

--- pages/api/send.js
+++ pages/api/send.js
@@ -5,11 +5,10 @@ export default async (req, res) => {
   if (req.method === "POST") {
     try {
       const response = await sendMail(req.body);
-      console.log(response);
     } catch (error) {
       console.log(error);
     }
   }
 
-  res.status(200).json(createEmail(req.body));
+  res.status(200).json({ status: "OK" });
 };
