4fa591d52fd12c850fee8c30e4b5756af2c8310d
Alex Kochnev <madjok87@icloud.com>
2023-10-20 13:11:02 +0200

    fix:slider

--- components/Projects/Swiper/SwiperVideo.js
+++ components/Projects/Swiper/SwiperVideo.js
@@ -1,102 +1,74 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Pagination, Thumbs } from "swiper/modules";
+import { Thumbs, Pagination } from "swiper/modules";
 import { VideoPlayer } from "../VideoPlayer";
-
+// import 'swiper/css/pagination';
 export const SwiperVideo = ({ videoSlides, poster }) => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const [showPagination, setShowPagination] = useState(true);
 
-  const updatePaginationVisibility = () => {
-    setShowPagination(window.innerWidth < 768);
-  };
 
   useEffect(() => {
+    const updatePaginationVisibility = () => {
+      if (window?.innerWidth) setShowPagination(window.innerWidth > 768);
+    };
     updatePaginationVisibility();
-
-    // Update visibility on window resize
     window.addEventListener("resize", updatePaginationVisibility);
 
     return () => {
       window.removeEventListener("resize", updatePaginationVisibility);
     };
   }, []);
-
-  const renderSwiperMobile = () => {
-    return (
+  return (
+    <>
       <Swiper
         slidesPerView={1}
         centeredSlides={true}
         loop={true}
-        modules={[Pagination]}
-        pagination={{ clickable: true }}
+        modules={[Thumbs, Pagination]}
+        pagination={!showPagination ? {
+          clickable: true,
+        } : false}
+        thumbs={{
+          swiper:
+            thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
       >
         {videoSlides.map((video, index) => (
           <SwiperSlide key={index}>
             <VideoPlayer poster={poster?.data} videofile={video} />
-            {/* Pagination */}
-            <div className="pagination-container py-2">
-              <span key={index}>&nbsp;</span>
-            </div>
           </SwiperSlide>
         ))}
       </Swiper>
-    );
-  };
-
-  const renderSwiperWideScreens = () => {
-    return (
-      <>
-        <Swiper
-          slidesPerView={1}
-          centeredSlides={true}
-          loop={true}
-          modules={[Thumbs]}
-          thumbs={{
-            swiper:
-              //thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-              thumbsSwiper,
-          }}
-        >
-          {videoSlides.map((video, index) => (
-            <SwiperSlide key={index}>
-              <VideoPlayer poster={poster?.data} videofile={video} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        {videoSlides.length > 1 && (
-          <div className="2xl:pt-12 xl:pt-12 lg:pt-7 pt-2 z-5 w-[90vw] mx-auto">
-            <Swiper
-              slidesPerView={2}
-              breakpoints={{
-                768: {
-                  spaceBetween: 30,
-                  slidesPerView: 2,
-                },
-                992: {
-                  slidesPerView: 3,
-                },
-              }}
-              spaceBetween={30}
-              scrollbar={{ draggable: true }}
-              loop={true}
-              onSwiper={setThumbsSwiper}
-            >
-              {videoSlides.map((video, index) => (
-                <SwiperSlide key={index}>
-                  <div className="rounded-lr overflow-hidden">
-                    <VideoPlayer poster={null} videofile={video} small={true} />
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-        )}
-      </>
-    );
-  };
-
-  return showPagination ? renderSwiperMobile() : renderSwiperWideScreens();
+      {videoSlides.length > 1 && (
+        <div className={`2xl:pt-12 xl:pt-12 lg:pt-7 pt-2 z-5 ${showPagination ? 'w-[90vw]' : 'w-0 h-0'} mx-auto`}>
+          <Swiper
+            slidesPerView={2}
+            breakpoints={{
+              768: {
+                spaceBetween: 30,
+                slidesPerView: 2,
+              },
+              992: {
+                slidesPerView: 3,
+              },
+            }}
+            spaceBetween={30}
+            scrollbar={{ draggable: true }}
+            loop={true}
+            onSwiper={setThumbsSwiper}
+          >
+            {videoSlides.map((video, index) => (
+              <SwiperSlide key={index}>
+                <div className="rounded-lr overflow-hidden">
+                  <VideoPlayer poster={null} videofile={video} small={true} />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </>
+  );
 };
