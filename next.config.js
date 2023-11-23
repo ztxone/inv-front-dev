f6a1dfdb3ccd66ac54e82681a4fd8bb19813744c
fgvlasov <fgvlasov@gmail.com>
2023-11-23 22:33:27 +0200

    domains

--- next.config.js
+++ next.config.js
@@ -11,6 +11,30 @@ module.exports = nextTranslate({
     domains: ["inv.ptzsite.ru", "localhost"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
+  async redirects() {
+    return [
+      {
+        source: "/?action=contacts",
+        destination: "/contacts",
+        permanent: true,
+      },
+      {
+        source: "/?action=visual",
+        destination: "/services/visual_ru",
+        permanent: true,
+      },
+      {
+        source: "/?action=animation",
+        destination: "/services/motion_video_ru",
+        permanent: true,
+      },
+      {
+        source: "/?action=portfolio",
+        destination: "/portfolio",
+        permanent: true,
+      },
+    ];
+  },
 });
 
 /*const nextConfig = {
