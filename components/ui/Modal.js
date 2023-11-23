81fba0ff1df1360470c196e815fe94259271bad5
fgvlasov <fgvlasov@gmail.com>
2023-11-23 22:50:18 +0200

    form

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -18,10 +18,10 @@ function Modal({ isOpen, onClose, children }) {
       <ReactModal
         isOpen={isOpen}
         onRequestClose={handleClose}
-        className="absolute top-20 z-50 mx-3.8 rounded-5xl md:max-w-[550px] md:mx-auto"
+        className="absolute top-10 z-50 mx-3.8 rounded-5xl md:max-w-[550px] md:mx-auto"
         overlayClassName="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-40
         flex flex-col items-center justify-center"
-        bodyOpenClassName="overflow-hidden"
+        bodyOpenClassName="!overflow-hidden"
       >
         <div className="modal-content  bg-black rounded-5xl">{children}</div>
         <ButtonClose onClick={handleClose} />
