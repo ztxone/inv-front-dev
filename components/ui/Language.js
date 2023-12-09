80de2aeeb4fa8e5ef6578f8b5574579102359112
fgvlasov <fgvlasov@gmail.com>
2023-12-09 15:00:30 +0200

    lang

--- components/ui/Language.js
+++ components/ui/Language.js
@@ -1,14 +1,18 @@
 import setLanguage from "next-translate/setLanguage";
 import RadioButton from "./Radio";
 import Line from "./Line";
+import { useRouter } from 'next/router';
 
 export default function Language({ lang }) {
+  const router = useRouter();
   const setLanguageRu = () => {
     if (lang === "en") setLanguage("ru");
+    router.push("/");
   };
 
   const setLanguageEn = () => {
     if (lang === "ru") setLanguage("en");
+    router.push("/en");
   };
   return (
     <div
@@ -59,37 +63,4 @@ export default function Language({ lang }) {
       </div>
     </div>
   );
-}
-
-{
-  /* <div
-className='flex flex-col items-center ml-auto pr-2.5 md:order-2 lg:order-none lg:ml-0
-lg:mr-auto trans-default'
->
-<button
-  type='button'
-  onClick={async () => await setLanguage('ru')}
-  className={`hover:text-suva-grey lg:block ${i18n.lang==='en'? ' mr-3.8':' text-suva-grey '
-    }`}
->
-  RU
-</button>
-<button
-  type='button'
-  onClick={async () => await setLanguage('en')}
-  className={`hover:text-suva-grey lg:block ${i18n.lang==='ru'? ' ml-3.8':' text-suva-grey'
-    }`}
->
-  EN
-</button>
-<svg
-  className='fill-gray ml-2.5 lg:hidden'
-  xmlns='http://www.w3.org/2000/svg'
-  width='10'
-  height='5'
-  viewBox='0 0 10 5'
->
-  <path fillRule='evenodd' clipRule='evenodd' d='m0 0 5 5 5-5H0Z' />
-</svg>
-</div> */
 }
