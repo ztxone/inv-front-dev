d49548fab33a6ab1b8743faaf730b29065d8b7be
Alex Kochnev <madjok87@icloud.com>
2024-02-20 14:40:17 +0100

    feat: add yandex metrika

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,12 @@
-import Document, {Html, Head, Main, NextScript} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head/>
+        <Head />
         <body>
-          <Script id="metrika-counter" strategy="afterInteractive">
-            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-            ym(50335891, "init", {
-                  defer: true,
-                  clickmap:true,
-                  trackLinks:true,
-                  accurateTrackBounce:true,
-				  ecommerce:"dataLayer",
-                  webvisor:true
-            });`
-            }
-          </Script>			
-          <noscript>
-            <div>
-              <img src="https://mc.yandex.ru/watch/50335891" style={{ position: 'absolute', left: '-9999px' }} alt="" />
-            </div>
-          </noscript>
           <Main />
           <NextScript />
           <Script src="//cdn.callibri.ru/callibri.js" type="text/javascript" />
