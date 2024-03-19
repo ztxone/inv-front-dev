373c5e294a2f5b4e3f3dbfc47aec80559437c529
fgvlasov <fgvlasov@gmail.com>
2024-03-19 14:43:41 +0200

    issues-3

--- next.config.js
+++ next.config.js
@@ -1,39 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextTranslate = require("next-translate-plugin");
+const nextTranslate = require('next-translate-plugin');
 
 module.exports = nextTranslate({
   reactStrictMode: false,
   //reactStrictMode: true,
   images: {
-    loader: "default",
-    domains: ["admin.invert.studio"],
+    loader: 'default',
+    domains: ['admin.invert.studio'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-  //   async redirects() {
-  //     return [
-  //       {
-  //         source: "/?action=contacts",
-  //         destination: "/contacts",
-  //         permanent: true,
-  //       },
-  //       {
-  //         source: "/?action=visual",
-  //         destination: "/services/visual_ru",
-  //         permanent: true,
-  //       },
-  //       {
-  //         source: "/?action=animation",
-  //         destination: "/services/motion_video_ru",
-  //         permanent: true,
-  //       },
-  //       {
-  //         source: "/?action=portfolio",
-  //         destination: "/portfolio",
-  //         permanent: true,
-  //       },
-  //     ];
-  //   },
 });
 
 /*const nextConfig = {
